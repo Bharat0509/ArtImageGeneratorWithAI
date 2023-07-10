8bfa920033edcbe613a94904c27230683e5ab304
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-07-10 16:26:29 +0530

    3

--- image-generator-server/server.js
+++ image-generator-server/server.js
@@ -2,9 +2,48 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import connectDB from "./mongodb/connect.js";
 import * as dotenv from "dotenv";
-import typeDefs from "./typeDefs.js";
+// import typeDefs from "./typeDefs.js";
 import resolvers from "./resolvers.js";
 dotenv.config();
+
+const typeDefs = `
+  type Img
+  { _id:String
+    name:String
+    prompt:String
+    image:String
+    public_id:String
+  }
+  type GeneratedImg
+  {
+    
+    prompt:String
+    img:String
+    
+  }
+  type PostData
+  {
+    url:String
+    name:String
+  }
+  type Query 
+  {
+    images:[Img]
+  }
+  type Mutation
+  {
+    generateImage(prompt:String):GeneratedImg
+    createPost(PostData:PostInput):PostData
+  }
+  input PostInput
+  {
+    name:String
+    prompt:String
+    image:String
+  }
+
+`;
+export default typeDefs;
 
 const server = new ApolloServer({
     typeDefs,
